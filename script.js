5c1976cc60868f9c0ea50d8679a062c99a222902
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-07-25 20:03:11 +0530

    added code for displaying output result and image

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ let birthDate = document.querySelector("#birth_date");
 
 let luckyNumber = document.querySelector("#lucky_number");
 
-submitButton = document.getElementById("submit_btn");
+let submitButton = document.getElementById("submit_btn");
+
+let displayOutput = document.getElementById("output");
+
+let happyImage = document.querySelector(".output_happy_image");
+
+// let happyImage = document.querySelector("#happy_image");
+
+let sadImage = document.querySelector(".output_sad_image");
+
 
 function birthDateHandler(event){
     event.preventDefault();
@@ -10,6 +19,8 @@ function birthDateHandler(event){
     let birthDateInput = birthDate.value;
 
     let luckyNumberInput = luckyNumber.value;
+
+    // let happyImageSrc = happyImage.value;
 
     let sum = 0;
 
@@ -24,7 +35,12 @@ function birthDateHandler(event){
     }
     
     if(sum % luckyNumberInput == 0){
-        console.log("Hurray, you are a lucky person!!!");
+        // console.log("Hurray, you are a lucky person!!!");
+
+        displayOutput.innerHTML += "Hurray, you are a lucky person🥳🥳🎉"+ "<br />" + "<img src = \"./images/undraw_Partying_re_at7f.svg\" width = \"60%\" height =\"40%\">";
+    }
+    else{
+        displayOutput.innerHTML += "You are not a lucky person" + "<br />" + "<img src = \"./images/undraw_feeling_blue_4b7q (1).svg\" width = \"60%\" height =\"40%\">";
     }
 }
 
